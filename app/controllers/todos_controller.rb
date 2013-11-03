a61ca58c08b4edf5f7a336c5742c5270fc00cab6
Rhiana Heath <rhiana@heath.cc>
2013-11-03 17:24:46 +1100

    Added flash messages, added styles to list

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -2,13 +2,11 @@ class TodosController < ApplicationController
   before_action :set_todo, only: [:show, :edit, :update, :destroy]
 
   # GET /todos
-  # GET /todos.json
   def index
     @todos = Todo.all
   end
 
   # GET /todos/1
-  # GET /todos/1.json
   def show
   end
 
@@ -22,42 +20,37 @@ class TodosController < ApplicationController
   end
 
   # POST /todos
-  # POST /todos.json
   def create
     @todo = Todo.new(todo_params)
 
     respond_to do |format|
       if @todo.save
-        format.html { redirect_to action: 'index', notice: 'To Do was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @todo }
+        flash[:notice] = 'To Do was successfully created.'
+        format.html { redirect_to action: 'index'}
       else
         format.html { render action: 'new' }
-        format.json { render json: @todo.errors, status: :unprocessable_entity }
       end
     end
   end
 
   # PATCH/PUT /todos/1
-  # PATCH/PUT /todos/1.json
   def update
     respond_to do |format|
       if @todo.update(todo_params)
-        format.html { redirect_to action: 'index', notice: 'To Do was successfully updated.' }
-        format.json { head :no_content }
+        flash[:notice] = 'To Do was successfully updated.'
+        format.html { redirect_to action: 'index'}
       else
         format.html { render action: 'edit' }
-        format.json { render json: @todo.errors, status: :unprocessable_entity }
       end
     end
   end
 
   # DELETE /todos/1
-  # DELETE /todos/1.json
   def destroy
     @todo.destroy
     respond_to do |format|
+      flash[:notice] = 'To Do was successfully deleted.'
       format.html { redirect_to todos_url }
-      format.json { head :no_content }
     end
   end
 
