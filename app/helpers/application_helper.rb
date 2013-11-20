e097b5b694688db9e438c9930ef1f6c56fe1723d
Rhiana Heath <rhiana@heath.cc>
2013-11-20 15:04:11 +1100

    styled new error, notice and success messgaes that can be closed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+
+    def flash_class(type)
+        case type
+        when :alert
+                "alert-info"
+        when :notice
+                "alert-success"
+        when :error
+        		"alert-danger"
+        else
+                ""
+        end
+	end
 end
